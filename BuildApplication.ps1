9e2a4974d1c51bba7b21de8b4e0fbd06ec8b05fd
Build Service <buildservice@mvpprojects.com>
2023-09-25 23:04:05 +0200

    update

--- BuildApplication.ps1
+++ BuildApplication.ps1
@@ -1,11 +1,13 @@
-if ($shortcutFile)
+$shortcutFilePath = "./WpfNotesSystemProg3.lnk"
+
+if (Test-Path -Path $shortcutFilePath)
 {
     remove-item $shortcutFile.FullName
 }
 else
 {
-    # $solutionFile = get-item ".\03_projects\WpfNotesSystem3\WpfNotesSystemProg3\WpfNotesSystemProg.sln"
-    # dotnet build $solutionFile.FullName
+    $solutionFile = get-item ".\03_projects\WpfNotesSystem3\WpfNotesSystemProg3\WpfNotesSystemProg.sln"
+    dotnet build $solutionFile.FullName
 }
 $shell = New-Object -comObject WScript.Shell
 $shortcut = $shell.CreateShortcut($shell.CurrentDirectory + "/" + "WpfNotesSystemProg3.lnk")
@@ -15,7 +17,7 @@ $shortcut.WorkingDirectory = $projectExeFile.Directory.FullName
 #$shortcut.Arguments = """\\machine\share\folder"""
 $shortcut.Save()
 
-$shortcutFile = get-item "./WpfNotesSystemProg3.lnk"
+$shortcutFile = get-item $shortcutFilePath
 Start-Process $shortcutFile.FullName
 
 Write-Host "Finish"
