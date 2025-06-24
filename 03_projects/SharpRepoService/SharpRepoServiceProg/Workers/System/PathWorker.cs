9d4b03540e56d258d9e80fc0d72b573e8b0ce074
Pawel Panda <pawelfluder@gmail.com>
2025-06-24 19:52:42 +0200

    update

--- 03_projects/SharpRepoService/SharpRepoServiceProg/Workers/System/PathWorker.cs
+++ 03_projects/SharpRepoService/SharpRepoServiceProg/Workers/System/PathWorker.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using SharpRepoServiceProg.Helpers;
 using SharpRepoServiceProg.Operations;
 using SharpRepoServiceProg.Registrations;
